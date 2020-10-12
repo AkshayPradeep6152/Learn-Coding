250da2fc6cc73d207d8776f43764b0a7c0e57926
S Balamurali <71454278+s-balamurali@users.noreply.github.com>
2020-10-12 21:41:50 +0530

    Create s-balamurali.java

---
+++ 5.Java/s-balamurali.java
@@ -0,0 +1,10 @@
+class s-balamurali {
+    public static void main(String[] args) {
+
+    String name = "S Balamurali";
+    int age =20 ;
+    String username = "s-balamurali";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
